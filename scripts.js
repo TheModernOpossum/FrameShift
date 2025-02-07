dccb2762eb440eaef7cb65ce40daf84e0102c3bf
Daniel <146178221+IthNaddaLithp@users.noreply.github.com>
2025-02-07 04:54:29 -0800

    project files for FrameShift

--- scripts.js
+++ scripts.js
@@ -6,3 +6,12 @@ module.exports = {
   },
   plugins: [],
 };
+
+document.getElementById("stateButton").addEventListener("click", function () {
+  this.innerText = "Clicked!";
+  this.disabled = true;
+  setTimeout(() => {
+    this.innerText = "Click Me";
+    this.disabled = false;
+  }, 2000);
+});
